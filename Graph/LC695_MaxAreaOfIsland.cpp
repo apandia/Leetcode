7080d23ba7388946fa0bbc7d7a2e4121086e61b4
apandia <96688353+apandia@users.noreply.github.com>
2022-06-04 14:05:28 +0530

    Create LC695_MaxAreaOfIsland.cpp

---
+++ Graph/LC695_MaxAreaOfIsland.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        if(grid.size() == 0)
+            return 0;
+        
+        int maxIsland = 0;
+        
+        for (int i = 0; i < grid.size() ; i++)
+        {
+            for(int j = 0; j < grid[i].size(); j++)
+            {
+                maxIsland = max(maxIsland, dfs(grid, i, j));
+            }
+        }
+        
+        return maxIsland;
+    }
+    
+    int dfs(vector<vector<int>>& grid, int i, int j)
+    {
+        if((i < 0) || (i >= grid.size()) || (j < 0) || (j >= grid[i].size()) || (grid[i][j] == 0))
+            return 0;
+        
+        grid[i][j] = 0;
+        
+        return 1 + dfs(grid, i-1, j) + dfs(grid, i+1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1);
+            
+    }
+};
