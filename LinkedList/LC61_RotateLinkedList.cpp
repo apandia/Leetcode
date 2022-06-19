5f9cec7e55b45375d512d25a1234e1d166cc7fd0
apandia <96688353+apandia@users.noreply.github.com>
2022-06-19 21:59:30 +0530

    Create LC61_RotateLinkedList.cpp

---
+++ LinkedList/LC61_RotateLinkedList.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == nullptr || head->next == nullptr || k == 0)
+            return head;
+        
+        ListNode* curr = head;
+        int listLength = 1;
+        while(curr->next)
+        {
+            curr = curr->next;
+            listLength++;
+        }
+        
+        curr->next = head;
+        
+        k = k % listLength;
+        k = listLength - k;
+        
+        while(k--)
+        {
+            curr = curr->next;
+        }
+        
+
+        head = curr->next;
+        curr->next = nullptr;
+        
+        return head;        
+    }
+};
